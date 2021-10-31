90ee7f8f08085325e8ac77462f950c528027f81a
pedroalves <pedrohcolsani@gmail.com>
2021-10-31 17:33:15 -0300

    criando react

---
+++ CardapioAPP/cardapio_app/src/Componentes/Pagina_Container.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Pagina_Container(){
+
+    return (
+        <>
+            <nav id = 'Menu'>
+                <ul>
+                    <li><a href='#'>Home</a></li>
+                    <li><a href='#'>Cadastro de Receitas</a></li>
+                    <li><a href='#'>Votação do Cardápio</a></li>
+                </ul>
+            </nav>
+        </>
+    )
+}
+
+export default Pagina_Container
