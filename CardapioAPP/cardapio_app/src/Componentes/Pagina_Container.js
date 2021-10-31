14d7af85ce0a8d759c8f90390a9365401080b66e
pedroalves <pedrohcolsani@gmail.com>
2021-10-31 19:18:23 -0300

    Fazendo o footer

--- CardapioAPP/cardapio_app/src/Componentes/Pagina_Container.js
+++ CardapioAPP/cardapio_app/src/Componentes/Pagina_Container.js
@@ -5,14 +5,22 @@ function Pagina_Container(){
 
     return (
         <>
-            <nav id = 'Menu'>
-                <ul id = 'Menu_List'>
-                    <li><img src ={Logo} alt = "Logo"/></li>
-                    <li><a href='#'>Home</a></li>
-                    <li><a href='#'>Cadastro de Receitas</a></li>
-                    <li><a href='#'>Votação do Cardápio</a></li>
-                </ul>
-            </nav>
+            <header>
+                <nav id = 'Menu'>
+                    <ul id = 'Menu_List'>
+                        <li><img src ={Logo} alt = "Logo"/></li>
+                        <li><a href='#'>Home</a></li>
+                        <li><a href='#'>Cadastro de Receitas</a></li>
+                        <li><a href='#'>Votação do Cardápio</a></li>
+                    </ul>
+                </nav>
+            </header>
+            <footer id = "Rodape">
+                <p>Gustavo de Cássio Vasselai</p>
+                <p>João Armando Alves Neto</p>
+                <p>Otávio Viñe Masella</p>
+                <p>Pedro Henrique Alves Silva</p>
+            </footer>
         </>
     )
 }
